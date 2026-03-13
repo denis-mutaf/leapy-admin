d60ad8aac53f7583470cad3c031ede1b16620da7
Denis Kosharnii <kosharnii@Deniss-MacBook-Pro.local>
2026-03-13 17:21:53 +0200

    Revert "feat(admin): Clerk auth + Supabase RLS"
    This reverts commit b62db1ab1ab2cd0c214a2275a445db7f7596c0c4.

--- app/sign-in/[[...sign-in]]/page.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-  return (
-    <div className="min-h-[60vh] flex items-center justify-center bg-page">
-      <SignIn />
-    </div>
-  );
-}
